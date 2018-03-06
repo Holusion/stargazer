d62fbb2140a94ae18cfbfc397e4887e9685240aa
Sebastien Dumetz <s.dumetz@holusion.com>
2018-03-06 15:15:47 +0100

    update event listener structure

--- BonjourListener.js
+++ BonjourListener.js
@@ -1,8 +1,32 @@
 'use strict';
-const {BonjourListener} = require('./build/Release/BonjourListener');
-
-console.log(BonjourListener);
-const browser = new BonjourListener(function(e){
-  console.log("callback called : ", e);
-});
-//browser.emit();
+const {NativeListener} = require('./build/Release/BonjourListener').BonjourListener;
+const {EventEmitter} = require("events");
+class BonjourListener extends EventEmitter{
+  constructor(){
+    super();
+    this._list = new Set();
+    this.browser = new NativeListener(this.onchange.bind(this));
+  }
+  get list(){
+    return Array.from(this._list);
+  }
+  onchange(str){
+    let changes = str.split("\n")
+    changes.forEach(function(line){
+      let [action, name] = line.split(" ");
+      switch (action){
+        case 'add':
+          this._list.add(name);
+          break;
+        case 'rm':
+          this._list.delete(name);
+          break;
+        default:
+          console.warn("unknown Zeroconf action : %s", action);
+      }
+    });
+    if (0 < changes.length){
+      this.emit("change", this.list);
+    }
+  }
+}
