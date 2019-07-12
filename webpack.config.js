bf0d5d473c16a2b5f0aafbf8c77467f085cd79c7
YannDub <yann59.dubois@gmail.com>
2019-07-12 16:49:16 +0200

    Add webpack and remove gulp

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    watch: true,
+    entry: "./src/index.js",
+    target: "electron-renderer",
+    mode: "development",
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: "babel-loader"
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
+        },
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "[name]-[hash:8].[ext]",
+                publicPath: "../dist"
+              }
+            }
+          ]
+        }
+      ]
+    },
+    resolve: { extensions: ["*", ".js", ".jsx"] },
+    output: {
+      path: path.resolve(__dirname, "dist/"),
+      publicPath: "/dist/",
+      filename: "bundle.js"
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()]
+  };
