7a5ca34a45677ed5a8e5631f50703c2e59651b20
YannDub <yann59.dubois@gmail.com>
2019-07-18 11:22:30 +0200

    Create unique id for Checkbox and Switch

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -5,10 +5,12 @@ import React from 'react'
 export default class Checkbox extends React.Component {
 
     render() {
+        const id = '_' + Math.random().toString(36).substr(2, 9);
+
         return (
             <div className="checkbox-container">
-                <input type="checkbox" className="checkbox" title={this.props.title} id="checkbox" />
-                <label htmlFor="checkbox" />
+                <input type="checkbox" className="checkbox" title={this.props.title} id={id} defaultChecked={false}/>
+                <label htmlFor={id} />
             </div>
         )
     }
--- src/components/Switch/Switch.js
+++ src/components/Switch/Switch.js
@@ -5,10 +5,12 @@ import React from 'react'
 export default class Switch extends React.Component {
 
     render() {
+        const id = '_' + Math.random().toString(36).substr(2, 9);
+
         return (
             <div className="switch-container">
-                <input type="checkbox" className="switch" title={this.props.title} id="switch" checked={this.props.checked} onChange={this.props.onChange} />
-                <label htmlFor="switch" />
+                <input type="checkbox" className="switch" title={this.props.title} id={id} defaultChecked={this.props.checked} onChange={this.props.onChange} />
+                <label htmlFor={id} />
             </div>
         )
     }
