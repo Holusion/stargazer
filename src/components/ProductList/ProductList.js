9b0d8c0ce4dcf6bacf6af2c5ecaa4b09dd7dc485
YannDub <yann59.dubois@gmail.com>
2019-08-07 10:35:02 +0200

    Add url logic in ProductList

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,9 +1,38 @@
 import {Button, List, ListItem} from '@holusion/react-components-holusion'
+import BadProductIPFound from '../../errors/BadProductIPFound'
 import PropTypes from 'prop-types'
 import React from 'react'
+import net from "net";
+
+function setUrl(product, props) {
+    let addr = null;
+    if(process.platform === "win32") {
+        for(let a of product.addresses) {
+            if(net.isIP(a) == 4) {
+                addr = a;
+            }
+        }
+
+        // Fetch can't parse ipv6 addresses
+        switch (net.isIP(addr)) {
+            case 6:
+                addr = `[${addr}]:3000`;
+                break;
+            case 0:
+                throw new BadProductIPFound(product.name, addr);
+            default:
+        }
+    } else {
+        addr = `${product.name}.local`;
+    }
+
+    if(addr) {
+        props.onProductSelected(addr);
+    }
+}
 
 export default function ProductList(props) {
-    const items = props.list.map(msg => <ListItem key={msg.name} icon="library" onClick={() => props.onListItemClick(msg)}>{msg.name}</ListItem>)
+    const items = props.list.map(msg => <ListItem key={msg.name} icon="library" onClick={() => setUrl(msg, props)}>{msg.name}</ListItem>)
 
     return (
         <div className={`left-content ${props.hide ? "hide" : ""}`}>
@@ -18,7 +47,7 @@ export default function ProductList(props) {
 }
 
 ProductList.propTypes = {
-    onListItemClick: PropTypes.func,
+    onProductSelected: PropTypes.func,
     onButtonClick: PropTypes.func,
     list: PropTypes.arrayOf(PropTypes.object),
     hide: PropTypes.bool,
